99bd681053f32a1ebf3240e455f8617b6a94f216
iyxan23 <github@iyxan.dev>
2025-11-30 22:07:04 +0700

    fix(board): sync chess piece positions with fixed coords

--- Board.java
+++ Board.java
@@ -1,40 +1,60 @@
 import greenfoot.*;
+import java.util.*;
 
 public class Board {
     public static int WIDTH = 8;
     public static int HEIGHT = 8;
 
     public ChessPiece[][] pieces = new ChessPiece[WIDTH][HEIGHT];
+    private ArrayList<ChessPiece> arrayPieces = new ArrayList<>();
 
     public Board() {}
 
     public static Board newStartingGame(int pieceSize) {
         Board board = new Board();
 
-        board.pieces[0][0] = new PieceRook(ChessPieceColor.WHITE, new BoardCoordinate(0, 0), pieceSize);
-        board.pieces[1][0] = new PieceKnight(ChessPieceColor.WHITE, new BoardCoordinate(0, 1), pieceSize);
-        board.pieces[2][0] = new PieceBishop(ChessPieceColor.WHITE, new BoardCoordinate(0, 2), pieceSize);
-        board.pieces[3][0] = new PieceQueen(ChessPieceColor.WHITE, new BoardCoordinate(0, 3), pieceSize);
-        board.pieces[4][0] = new PieceKing(ChessPieceColor.WHITE, new BoardCoordinate(0, 4), pieceSize);
-        board.pieces[5][0] = new PieceBishop(ChessPieceColor.WHITE, new BoardCoordinate(0, 5), pieceSize);
-        board.pieces[6][0] = new PieceKnight(ChessPieceColor.WHITE, new BoardCoordinate(0, 6), pieceSize);
-        board.pieces[7][0] = new PieceRook(ChessPieceColor.WHITE, new BoardCoordinate(0, 7), pieceSize);
+        board.pieces[0][0] = new PieceRook(ChessPieceColor.WHITE,   new BoardCoordinate(0, 0), pieceSize);
+        board.pieces[1][0] = new PieceKnight(ChessPieceColor.WHITE, new BoardCoordinate(1, 0), pieceSize);
+        board.pieces[2][0] = new PieceBishop(ChessPieceColor.WHITE, new BoardCoordinate(2, 0), pieceSize);
+        board.pieces[3][0] = new PieceQueen(ChessPieceColor.WHITE,  new BoardCoordinate(3, 0), pieceSize);
+        board.pieces[4][0] = new PieceKing(ChessPieceColor.WHITE,   new BoardCoordinate(4, 0), pieceSize);
+        board.pieces[5][0] = new PieceBishop(ChessPieceColor.WHITE, new BoardCoordinate(5, 0), pieceSize);
+        board.pieces[6][0] = new PieceKnight(ChessPieceColor.WHITE, new BoardCoordinate(6, 0), pieceSize);
+        board.pieces[7][0] = new PieceRook(ChessPieceColor.WHITE,   new BoardCoordinate(7, 0), pieceSize);
 
-        board.pieces[0][7] = new PieceRook(ChessPieceColor.BLACK, new BoardCoordinate(7, 0), pieceSize);
-        board.pieces[1][7] = new PieceKnight(ChessPieceColor.BLACK, new BoardCoordinate(7, 1), pieceSize);
-        board.pieces[2][7] = new PieceBishop(ChessPieceColor.BLACK, new BoardCoordinate(7, 2), pieceSize);
-        board.pieces[3][7] = new PieceQueen(ChessPieceColor.BLACK, new BoardCoordinate(7, 3), pieceSize);
-        board.pieces[4][7] = new PieceKing(ChessPieceColor.BLACK, new BoardCoordinate(7, 4), pieceSize);
-        board.pieces[5][7] = new PieceBishop(ChessPieceColor.BLACK, new BoardCoordinate(7, 5), pieceSize);
-        board.pieces[6][7] = new PieceKnight(ChessPieceColor.BLACK, new BoardCoordinate(7, 6), pieceSize);
-        board.pieces[7][7] = new PieceRook(ChessPieceColor.BLACK, new BoardCoordinate(7, 7), pieceSize);
+        board.pieces[0][7] = new PieceRook(ChessPieceColor.BLACK,   new BoardCoordinate(0, 0), pieceSize);
+        board.pieces[1][7] = new PieceKnight(ChessPieceColor.BLACK, new BoardCoordinate(1, 0), pieceSize);
+        board.pieces[2][7] = new PieceBishop(ChessPieceColor.BLACK, new BoardCoordinate(2, 0), pieceSize);
+        board.pieces[3][7] = new PieceQueen(ChessPieceColor.BLACK,  new BoardCoordinate(3, 0), pieceSize);
+        board.pieces[4][7] = new PieceKing(ChessPieceColor.BLACK,   new BoardCoordinate(4, 0), pieceSize);
+        board.pieces[5][7] = new PieceBishop(ChessPieceColor.BLACK, new BoardCoordinate(5, 0), pieceSize);
+        board.pieces[6][7] = new PieceKnight(ChessPieceColor.BLACK, new BoardCoordinate(6, 0), pieceSize);
+        board.pieces[7][7] = new PieceRook(ChessPieceColor.BLACK,   new BoardCoordinate(7, 0), pieceSize);
 
         for (int i = 0; i < WIDTH; i++) {
             board.pieces[i][1] = new PiecePawn(ChessPieceColor.WHITE, new BoardCoordinate(i, 1), pieceSize);
             board.pieces[i][6] = new PiecePawn(ChessPieceColor.BLACK, new BoardCoordinate(i, 6), pieceSize);
         }
 
+        board.syncPieces();
+
         return board;
+    }
+
+    protected void syncPieces() {
+        this.arrayPieces.clear();
+
+        for (int x = 0; x < WIDTH; x++) {
+            for (int y = 0; y < HEIGHT; y++) {
+                if (this.pieces[x][y] != null) {
+                    this.arrayPieces.add(this.pieces[x][y]);
+                }
+            }
+        }
+    }
+
+    public ChessPiece[] getAllPieces() {
+        return this.arrayPieces.toArray(new ChessPiece[0]);
     }
 
     public void setPiece(ChessPiece piece, int x, int y) {
@@ -47,5 +67,9 @@ public class Board {
 
     public boolean hasPiece(int x, int y) {
         return pieces[x][y] != null;
+    }
+
+    public static boolean inBounds(int x, int y) {
+        return x >= 0 && x < Board.WIDTH && y >= 0 && y < Board.HEIGHT;
     }
 }
